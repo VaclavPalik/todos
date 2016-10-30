2f5359173f600331b28b6ca45e3ed90ffdb29d05
Vaclav Palik <vaclavpalik@gmail.com>
2016-10-30 10:15:25 +0100

    Create Todo model class

---
+++ src/io/github/vaclavpalik/todos/model/Todo.java
@@ -0,0 +1,51 @@
+package io.github.vaclavpalik.todos.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Todo {
+	private static int counter = 0;
+
+	private int id = ++counter;
+	private LocalDateTime createdOn = LocalDateTime.now();
+	private String description;
+	private LocalDate resolveUntil;
+	private boolean resolved = false;
+
+	public Todo(String description, LocalDate resolveUntil) {
+		this.description = description;
+		this.resolveUntil = resolveUntil;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getResolveUntil() {
+		return resolveUntil;
+	}
+
+	public void setResolveUntil(LocalDate resolveUntil) {
+		this.resolveUntil = resolveUntil;
+	}
+
+	public boolean isResolved() {
+		return resolved;
+	}
+
+	public void setResolved(boolean resolved) {
+		this.resolved = resolved;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public LocalDateTime getCreatedOn() {
+		return createdOn;
+	}
+}
